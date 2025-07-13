604611571bff51ec5a5acb1e8182454acf7b3e6f
Bartek <prozariuss@gmail.com>
2025-07-13 14:21:34 +0200

    Dodanie obrazków

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     publish_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(null=True, blank=True, upload_to='image/')
 
     def publish(self):
         self.publish_date = timezone.now()
